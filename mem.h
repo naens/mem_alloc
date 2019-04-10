76305733026a27b8d2165c5749a63789cd7bfbea
naens <naens@gmx.com>
2019-04-10 10:11:53 +0200

    mem.h: update for use with IDE

--- mem.h
+++ mem.h
@@ -1,9 +1,9 @@
 #ifndef MEM_H
 #define MEM_H
 
-void mem_init();
-void mem_finalize();
+void mem_init(void);
+void mem_finalize(void);
 void *mem_alloc(unsigned int x);
-void *mem_free(void *area);
+void mem_free(void *area);
 
 #endif /* MEM_H */
